2d969ff2cb9f6ebdcf517ef644db548f7cdd5b08
Edward Andrei <edwardandrei192@gmail.com>
2026-04-18 12:58:13 +0200

    enhance user navigation with dynamic login/logout links and username display

--- src/main/java/group02/filmout/controller/MainController.java
+++ src/main/java/group02/filmout/controller/MainController.java
@@ -1,21 +1,26 @@
 package group02.filmout.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import group02.filmout.entity.User;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
+
   @GetMapping("/")
   public String root() {
-
     return "redirect:/home";
   }
 
   @GetMapping("/home")
-  public String home() {
-      
+  public String home(HttpSession session, Model model) {
+    User loggedUser = (User) session.getAttribute("loggedUser");
+    if (loggedUser != null) {
+      model.addAttribute("loggedUser", loggedUser);
+    }
     return "index";
   }
-  
 }
